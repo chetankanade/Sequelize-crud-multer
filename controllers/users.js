7d0a7f1504be8c3657441fecd72b01238578344a
chetan kanade <chetankanade8@gmail.com>
2023-06-19 00:49:52 +0530

    update in updateUser

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const { hashpassword, token, verifypass } = require("../utils/utils");
 
 exports.signup = async (req, res) => {
+  console.log(this.signup);
   try {
     let { firstName, email, password } = req.body;
     const isUser = await User.findOne({
@@ -61,14 +62,23 @@ exports.login = async (req, res) => {
 
 exports.updateProfile = async (req, res) => {
   try {
-    let { firstName, email, password, image } = req.body;
+    let { firstName, lastName, email, password, image } = req.body;
     let { id } = req.user;
+    console.log(req.user.id);
 
     const user = await User.findOne({ where: { id } });
     if (!user) {
       throw new Error("user not found");
     }
+    let updateData = {
+      firstName,
+      email,
+      password,
+      image,
+    };
 
+    let update = await User.update({ updateData }, { where: { id } });
+    if (!update) throw new Error();
     res.status(200).json({ status: true, msg: "success", user });
   } catch (err) {
     res.status(400).json({ status: false, msg: err.message });
